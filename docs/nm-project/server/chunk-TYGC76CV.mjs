1ff52408a11715b14f238307c4c84ac1b29c69fe
Irais <irais.fdz@gmail.com>
2024-10-01 14:40:01 -0500

    docs build

---
+++ docs/nm-project/server/chunk-TYGC76CV.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import"./chunk-5XUXGTUW.mjs";var t=[{path:"home",loadComponent:()=>import("./chunk-OWTQ4MIX.mjs").then(o=>o.HomeComponent)},{path:"",redirectTo:"/home",pathMatch:"full"}];export{t as routes};
---
+++ docs/nm-project/server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-IBJTIC3G.mjs";import"./chunk-LXDI7C6K.mjs";import"./chunk-B652OGAO.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
