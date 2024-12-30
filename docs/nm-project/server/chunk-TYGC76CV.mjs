dcb99e376cb6103635ae20a41071bdf88daad44f
Irais <irais.fdz@gmail.com>
2024-12-29 22:55:08 -0600

    update w latest angular

--- docs/nm-project/server/chunk-TYGC76CV.mjs
+++
@@ -1,2 +0,0 @@
-import './polyfills.server.mjs';
-import"./chunk-5XUXGTUW.mjs";var t=[{path:"home",loadComponent:()=>import("./chunk-OWTQ4MIX.mjs").then(o=>o.HomeComponent)},{path:"",redirectTo:"/home",pathMatch:"full"}];export{t as routes};
--- docs/nm-project/server/main.server.mjs
+++ docs/nm-project/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-IBJTIC3G.mjs";import"./chunk-LXDI7C6K.mjs";import"./chunk-B652OGAO.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-LWZHRXP4.mjs";import"./chunk-GV73MXT3.mjs";import"./chunk-K4NNPURR.mjs";import"./chunk-R7LT3SW3.mjs";import"./chunk-AD5JPUVZ.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
