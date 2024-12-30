7d1b88e1c3d6c53b0d92982adad946e5fe24eb3b
Irais <irais.fdz@gmail.com>
2024-12-29 23:50:53 -0600

    Auto-generated commit

--- chunk-J76V52N3.js
+++
@@ -1,1 +0,0 @@
-import"./chunk-7CGTOI24.js";var t=[{path:"home",loadComponent:()=>import("./chunk-34A3G5SI.js").then(o=>o.HomeComponent)},{path:"",redirectTo:"/home",pathMatch:"full"}];export{t as routes};
