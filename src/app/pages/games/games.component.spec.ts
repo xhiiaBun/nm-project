bf33c3b935d8b527006deacd5079f497da2d9b91
Irais <irais.fdz@gmail.com>
2025-01-04 22:24:01 -0600

    update background

--- src/app/pages/games/games.component.spec.ts
+++ src/app/pages/games/games.component.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { BookcaseComponent } from './games.component';
+import { GamesComponent } from './games.component';
 
-describe('BookcaseComponent', () => {
-  let component: BookcaseComponent;
-  let fixture: ComponentFixture<BookcaseComponent>;
+describe('GamesComponent', () => {
+  let component: GamesComponent;
+  let fixture: ComponentFixture<GamesComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [BookcaseComponent]
+      imports: [GamesComponent]
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(BookcaseComponent);
+    fixture = TestBed.createComponent(GamesComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
