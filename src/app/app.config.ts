a773be60ec738129c476994883871cd990cf0ec5
Irais <irais.fdz@gmail.com>
2025-01-15 12:36:17 -0600

    animate decreasing attempt

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,4 +1,4 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
@@ -7,9 +7,11 @@ import { provideClientHydration } from '@angular/platform-browser';
 import { provideHttpClient } from '@angular/common/http';
 import { withInterceptors } from '@angular/common/http';
 import { serverErrorInterceptor } from './interceptors/server-error.interceptor';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
   providers: [provideZoneChangeDetection({ eventCoalescing: true }), 
     provideRouter(routes), provideClientHydration(),
-    provideHttpClient(withInterceptors([serverErrorInterceptor]))]
+    provideHttpClient(withInterceptors([serverErrorInterceptor])),
+    importProvidersFrom(BrowserAnimationsModule)]
 };
--- src/app/pages/games/guessword/paint-figure/paint-figure.component.ts
+++ src/app/pages/games/guessword/paint-figure/paint-figure.component.ts
@@ -1,18 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 import { WordService } from '../../services/get-word-service.service';
+import { animate, state, style, transition, trigger } from '@angular/animations';
 
 @Component({
   selector: 'paint-figure',
   standalone: true,
   imports: [],
   templateUrl: './paint-figure.component.html',
-  styleUrl: './paint-figure.component.css'
+  styleUrl: './paint-figure.component.css',
+  animations: [
+    trigger('colorTransition', [
+      transition(':decrement', [animate('1s ease-in-out', 
+        style({ color: '#E91E63', fontSize: '2rem'})), 
+        animate('1s ease-in-out', style({fontSize: '1.3rem'}))])
+      ]
+    )
+  ]
 })
 export class PaintFigureComponent implements OnInit{
   sharedVal: any;
   magic_word: string = "";
   mg_len: number = 0;
   numberAttempts: number = 0;
+  imageView: boolean = true;
 
   constructor(private _wordService: WordService){}
 
