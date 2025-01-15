45153a12be34769a984b9965db592db57d6a672f
Irais <irais.fdz@gmail.com>
2025-01-14 19:55:19 -0600

    Auto-generated commit

---
+++ chunk-XI6KKVYF.js
@@ -0,0 +1,1 @@
+import{Ea as n,P as a}from"./chunk-ZETZ2MQH.js";var t=class e{isGameZoneExpanded=n(!1);constructor(){}setisGameZoneExpanded(o){this.isGameZoneExpanded.set(o)}getisGameZoneExpanded(){return this.isGameZoneExpanded()}static \u0275fac=function(i){return new(i||e)};static \u0275prov=a({token:e,factory:e.\u0275fac,providedIn:"root"})};export{t as a};
